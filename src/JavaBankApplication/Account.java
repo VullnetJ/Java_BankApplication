49bddde19fc1287c181b09906b551e06128e498d
VullnetJ <46821353+VullnetJ@users.noreply.github.com>
2022-02-04 11:04:09 +0200

    Update Account.java

--- src/JavaBankApplication/Account.java
+++ src/JavaBankApplication/Account.java
@@ -1,9 +1,5 @@
 package JavaBankApplication;
 
-// ************************************************************************
-// Account.java	  Template created on15.9.2016
-// - The class for Account objects
-// ************************************************************************
 public class Account {
 	// Fields
 
